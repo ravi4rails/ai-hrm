a94e0737027354ad9df539fa3bdbc357b605319e
ravi4rails <ravionrails@gmail.com>
2019-10-19 15:00:51 +0530

    quick fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_19_072138) do
+ActiveRecord::Schema.define(version: 2019_10_09_055515) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2019_10_19_072138) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.date "established_from"
   end
 
   create_table "educational_credentials", force: :cascade do |t|
